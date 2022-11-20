65741b25d5c3cf31eff562332c21ff5475a11727
AnkitaGoyalD2004 <117336913+AnkitaGoyalD2004@users.noreply.github.com>
2022-11-20 09:15:54 +0530

    Update MaxSubArray.java

--- src/arrays/MaxSubArray.java
+++ src/arrays/MaxSubArray.java
@@ -14,8 +14,4 @@ public class MaxSubArray {
 	        }
 	        return Compmax;
 	 }
-	 public static void main(String[] args) {
-			int arr[] = {-2,1,-3,4,-1,2,1,-5,4};
-			System.out.print(maxSubArray(arr));
-		}
 }
