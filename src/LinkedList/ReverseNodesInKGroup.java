29732d24f9995f1cb3b5e41dba1e9799491b5107
AnkitaGoyalD2004 <117336913+AnkitaGoyalD2004@users.noreply.github.com>
2022-11-26 11:17:20 +0530

    Update ReverseNodesInKGroup.java

--- src/LinkedList/ReverseNodesInKGroup.java
+++ src/LinkedList/ReverseNodesInKGroup.java
@@ -1,5 +1,30 @@
 package LinkedList;
 
+class ListNode {
+	      int val;
+	      ListNode next;
+	      ListNode() {}
+	      ListNode(int val) { this.val = val; }
+	      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+	  }
 public class ReverseNodesInKGroup {
+	public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode curr = head;
+        ListNode temp = head;
+        ListNode prev = null;
+        for(int i=0;i<k;i++,  temp=temp.next){
+            //TEMP=TEMP.next; 
+            if(temp==null)
+                return head;   
+        }
+        for(int i=0;i<k;i++){
+            temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        head.next = reverseKGroup(curr, k);
+        return prev;
+  }
 
 }
