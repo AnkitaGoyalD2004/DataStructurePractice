2cdb051acd23470ca0b1a0f6e8ec3d1892af0d25
AnkitaGoyalD2004 <117336913+AnkitaGoyalD2004@users.noreply.github.com>
2022-12-05 09:07:47 +0530

    Add files via upload

---
+++ src/BinarySearch/LowestCommonAncestorInBst.java
@@ -0,0 +1,20 @@
+package Tress;
+
+class TreeNode {
+	      int val;
+	      TreeNode left;
+	      TreeNode right;
+	      TreeNode(int x) { val = x; }
+	  }
+public class LowestCommonAncestorInBst {
+
+		    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+		        if(p.val < root.val && q.val < root.val){
+		        return lowestCommonAncestor(root.left,p,q);}
+		        else if(p.val > root.val && q.val > root.val){
+		        return lowestCommonAncestor(root.right,p,q);}
+		        else{
+		        return root;
+		    }
+		}
+		}
