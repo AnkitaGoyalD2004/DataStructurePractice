404a981536bb580da69cb81ea6a0a874e90eaf8e
AnkitaGoyalD2004 <117336913+AnkitaGoyalD2004@users.noreply.github.com>
2023-03-01 16:00:15 +0530

    Done on leetcode(264)

---
+++ src/DynamicProgramming/UglyNumberII.java
@@ -0,0 +1,22 @@
+package DynamicProgramming;
+public class UglyNumberII {
+    public int nthUglyNumber(int n) {
+        // using DP
+        int dp[] = new int[n + 1];
+        dp[0] = 1;
+        int i2 = 0, i3 = 0, i5 = 0;
+        for (int i = 1; i < dp.length; i++) {
+            dp[i] = Math.min(dp[i2] * 2, Math.min(dp[i3] * 3, dp[i5] * 5));
+            if (dp[i] == dp[i2] * 2) {
+                i2++;
+            }
+            if (dp[i] == dp[i3] * 3) {
+                i3++;
+            }
+            if (dp[i] == dp[i5] * 5) {
+                i5++;
+            }
+        }
+        return dp[n - 1];
+    }
+}
