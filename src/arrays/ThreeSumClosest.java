e17d422d33965ce3911593559ebe52fe35a2e795
AnkitaGoyalD2004 <117336913+AnkitaGoyalD2004@users.noreply.github.com>
2023-03-21 07:13:10 +0530

    done on leetcode(16)

---
+++ src/arrays/ThreeSumClosest.java
@@ -0,0 +1,26 @@
+package Arrays;
+import java.util.Arrays;
+public class ThreeSumClosest {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int ans=0;
+        int diff = Integer.MAX_VALUE;
+        for(int i =0;i<nums.length;i++){
+            int first = nums[i]; 
+            int start = i+1;
+            int end = nums.length-1;
+            while(start<end){
+                if(first + nums[start] + nums[end] == target) return target;
+                 else if(Math.abs(first+nums[start]+nums[end]-target)< diff){
+                    diff = Math.abs(first+nums[start]+nums[end]-target);
+                        ans = first+nums[start]+nums[end];
+                }
+                if(first + nums[start] + nums[end] > target) end--;
+                else start++;
+            }  
+        } 
+        return ans; 
+    }
+    
+    
+}
