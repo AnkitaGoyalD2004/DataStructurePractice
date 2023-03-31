101b1dc187d5fe64ed0728057bfcea5b50324e1d
AnkitaGoyalD2004 <117336913+AnkitaGoyalD2004@users.noreply.github.com>
2023-03-31 11:07:19 +0530

    done on leetcode(496)

---
+++ src/HashMap/NextGreaterElement1.java
@@ -0,0 +1,20 @@
+package HashMap;
+import java.util.Stack;
+import java.util.HashMap;
+public class NextGreaterElement1 {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> stack = new Stack<>();
+        HashMap<Integer,Integer> h = new HashMap<>();
+        
+        for(int num : nums2){
+            while(!stack.isEmpty() && stack.peek()<num){
+                h.put(stack.pop(),num);
+            }
+            stack.push(num);
+        }
+        for(int i =0; i<nums1.length ; i++){
+            nums1[i] = h.getOrDefault(nums1[i], -1);
+        }
+        return nums1;
+    }
+}
