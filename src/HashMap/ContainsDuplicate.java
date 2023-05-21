ef0f3bf4d6c9e641a0a278e6e8231ddb971af706
AnkitaGoyalD2004 <117336913+AnkitaGoyalD2004@users.noreply.github.com>
2023-05-21 21:44:02 +0530

    done on leetcode (217)

---
+++ src/HashMap/ContainsDuplicate.java
@@ -0,0 +1,16 @@
+package HashMap;
+
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> hs = new HashSet<Integer>();
+        for (int i = 0; i < nums.length; i ++){
+            if (hs.contains(nums[i])){
+                return true;
+            }
+            hs.add(nums[i]);
+        }
+        return false;
+    }
+}
