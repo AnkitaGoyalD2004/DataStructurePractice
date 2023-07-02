25285a57d9b14938cbc882a0d832d319d3cf774e
AnkitaGoyalD2004 <117336913+AnkitaGoyalD2004@users.noreply.github.com>
2023-07-02 23:23:48 +0530

    Add files via upload

---
+++ src/Backtracking/ArchievableTransferRequest.java
@@ -0,0 +1,33 @@
+package Backtracking;
+
+public class ArchievableTransferRequest {
+    int max = Integer.MIN_VALUE;
+
+    public int maximumRequests(int n, int[][] requests) {
+
+        helper(requests, new int[n], 0, 0);
+        return max;
+    }
+
+    public void helper(int[][] requests, int[] buildingArr, int reqCount, int ind) {
+
+        if (ind >= requests.length) {
+
+            for (int building : buildingArr) {
+                if (building != 0) {
+                    return;
+                }
+            }
+            max = Math.max(max, reqCount);
+            return;
+        }
+
+        buildingArr[requests[ind][0]]--;
+        buildingArr[requests[ind][1]]++;
+        helper(requests, buildingArr, reqCount + 1, ind + 1);
+        buildingArr[requests[ind][0]]++;
+        buildingArr[requests[ind][1]]--;
+
+        helper(requests, buildingArr, reqCount, ind + 1);
+    }
+}
