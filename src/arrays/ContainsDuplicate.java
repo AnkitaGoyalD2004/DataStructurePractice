40d2587cd481dd0d7a4508942c1c67e44a06541f
AnkitaGoyalD2004 <117336913+AnkitaGoyalD2004@users.noreply.github.com>
2023-07-10 10:27:20 +0530

    done on leetcode 217

---
+++ src/arrays/ContainsDuplicate.java
@@ -0,0 +1,13 @@
+package Arrays;
+import java.util.Arrays;
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+      Arrays.sort(nums);
+      for(int i = 0;i<nums.length-1;i++){
+        if(nums[i] == nums[i+1]){
+            return true;
+        }
+      }
+return false;
+    }
+}
