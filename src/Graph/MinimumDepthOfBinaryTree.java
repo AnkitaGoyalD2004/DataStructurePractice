e094e7a76161c27a27122940384a8edd8fb452e4
AnkitaGoyalD2004 <117336913+AnkitaGoyalD2004@users.noreply.github.com>
2023-07-10 21:18:17 +0530

    done on leetcode

---
+++ src/Graph/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,27 @@
+package Graph;
+
+public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+public class MinimumDepthOfBinaryTree {
+    public int minDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }else if(root.left == null){
+            return 1+ minDepth(root.right);
+        }else if(root.right == null){
+            return 1+minDepth(root.left);
+        }
+            return 1 + Math.min(minDepth(root.left) , minDepth(root.right));
+    }
+}
