191cf81fa17f8f7897006c303ae83c94e64a6413
AnkitaGoyalD2004 <117336913+AnkitaGoyalD2004@users.noreply.github.com>
2023-07-30 07:25:48 +0530

    Update CeilOfAnElementSortedArray.java

--- src/AdityaVermaBinarySearch/CeilOfAnElementSortedArray.java
+++ src/AdityaVermaBinarySearch/CeilOfAnElementSortedArray.java
@@ -1,6 +1,7 @@
 package AdityaVermaBinarySearch;
 
 public class CeilOfAnElementSortedArray {
+	
 	public static int findCeil(int[] arr, int n, int x) {
 		int result = -1;
 		int start = 0,end = n-1;
