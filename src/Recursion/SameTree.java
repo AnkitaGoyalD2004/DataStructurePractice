a258313aecc66932045a789fdb4671b2f780aa06
AnkitaGoyalD2004 <117336913+AnkitaGoyalD2004@users.noreply.github.com>
2023-08-07 22:17:23 +0530

    done on leetcode(100)

---
+++ src/Recursion/SameTree.java
@@ -0,0 +1,25 @@
+package Recursion;
+public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+         if(p == null && q == null){
+            return true;
+         }
+         if(p == null || q == null || p.val != q.val){
+            return false;
+         }
+         return isSameTree(p.left, q.left) && isSameTree(p.right , q.right);
+
+    }
+}
