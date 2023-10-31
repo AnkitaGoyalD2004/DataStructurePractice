583ef6768b189381e789361cebd9894c96b9fc5c
AnkitaGoyalD2004 <117336913+AnkitaGoyalD2004@users.noreply.github.com>
2023-10-31 18:06:09 +0530

    done on leetcode 2433

---
+++ src/BitManipulation/FindTheOriginalArray.java
@@ -0,0 +1,16 @@
+package BitManipulation;
+
+public class FindTheOriginalArray {
+    public static void main(String[] args){
+
+    }
+    public int[] findArray(int[] pref){
+        int[] result = new int[pref.length];
+        result[0] = pref[0];
+
+        for(int i = 1 ; i < pref.length ; i++){
+            result[i] = pref[i] ^ pref[i-1];
+        }
+        return result;
+    }
+}
